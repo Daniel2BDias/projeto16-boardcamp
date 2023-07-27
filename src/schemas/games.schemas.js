09519b13d3fec35c6d981bef669dd9c0f24c26a5
Daniel Dias <daniiel2dias@gmail.com>
2023-07-27 13:13:05 -0300

    removed validation error

--- src/schemas/games.schemas.js
+++ src/schemas/games.schemas.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const gameSchema = joi.object().keys({
     name: joi.string().required(),
-    image: joi.string().uri().domain().required(),
+    image: joi.string().uri().required(),
     stockTotal: joi.number().integer().min(1).required(),
     pricePerDay: joi.number().integer().min(1).required(),
   });
