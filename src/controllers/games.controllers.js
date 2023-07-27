f097a5b4d7a5157559df0d3f4a98d721a084ed4d
Daniel Dias <daniiel2dias@gmail.com>
2023-07-27 13:21:32 -0300

    test

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -15,7 +15,7 @@ export const registerNewGame = async (req, res) => {
 
     try{
         const alreadyRegistered = await db.query(`SELECT * FROM games WHERE name=$1`, [name]);
-        if(alreadyRegistered) return res.sendStatus(409);
+        if(alreadyRegistered) return res.status(409).send(alreadyRegistered);
 
         await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay]);
         res.sendStatus(201); 
