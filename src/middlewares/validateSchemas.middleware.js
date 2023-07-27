0ad1b6b48e7784956db5c0d6a802d8e9567483a3
Daniel Dias <daniiel2dias@gmail.com>
2023-07-27 12:52:30 -0300

    erros técnicos corrigidos

--- src/middlewares/validateSchemas.middleware.js
+++ src/middlewares/validateSchemas.middleware.js
@@ -1,10 +1,7 @@
-import joi from "joi";
-
-
-export default function validateSchema (schema) {
-    return (req, res, next) => {
-        const {error} = joi.validate(req.body, schema);
-        if(error) return res.sendStatus(400);
-        next();
-    }
-};
+export default function validateSchema(schema) {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) return res.sendStatus(400);
+    next();
+  };
+}
