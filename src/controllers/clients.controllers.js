15b86bcf317f1c8600b8668120fad54d5a1d6190
Daniel Dias <daniiel2dias@gmail.com>
2023-07-27 19:20:47 -0300

    last commit but formatted

--- src/controllers/clients.controllers.js
+++ src/controllers/clients.controllers.js
@@ -15,7 +15,7 @@ export const getClientById = async (req, res) => {
     const clientById = await db.query(`SELECT * FROM costumers WHERE id=$1;`, [
       id,
     ]);
-    if(!clientById) return res.sendStatus(404);
+    if (!clientById) return res.sendStatus(404);
     res.status(200).send(clientById.rows);
   } catch (error) {
     res.status(500).send(error.message);
@@ -23,12 +23,18 @@ export const getClientById = async (req, res) => {
 };
 
 export const registerNewClient = async (req, res) => {
-  const {name, cpf, birthday, phone} = req.body;
+  const { name, cpf, birthday, phone } = req.body;
   try {
-    const alreadyRegistered = await db.query(`SELECT * FROM customers WHERE cpf=$1;`, [cpf]);
-    if(alreadyRegistered?.rows[0]?.name) return res.sendStatus(409);
+    const alreadyRegistered = await db.query(
+      `SELECT * FROM customers WHERE cpf=$1;`,
+      [cpf]
+    );
+    if (alreadyRegistered?.rows[0]?.name) return res.sendStatus(409);
 
-    await db.query(`INSERT INTO customers (name, cpf, birthday, phone) VALUES ($1, $2, $3, $4);`, [name, cpf, birthday, phone]);
+    await db.query(
+      `INSERT INTO customers (name, cpf, birthday, phone) VALUES ($1, $2, $3, $4);`,
+      [name, cpf, birthday, phone]
+    );
     res.sendStatus(201);
   } catch (error) {
     res.status(500).send(error.message);
@@ -40,10 +46,16 @@ export const editClientRegistry = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const cpfConflict = await db.query(`SELECT * FROM customers WHERE cpf=$1;`, [cpf]);
-    if(cpfConflict?.rows?.cpf) return res.sendStatus(409);
+    const cpfConflict = await db.query(
+      `SELECT * FROM customers WHERE cpf=$1;`,
+      [cpf]
+    );
+    if (cpfConflict?.rows?.cpf) return res.sendStatus(409);
 
-    await db.query(`UPDATE customers SET name=$2, cpf=$3, birthday=$4, phone=$5 WHERE id=$1;`, [id, name, cpf, birthday, phone]);
+    await db.query(
+      `UPDATE customers SET name=$2, cpf=$3, birthday=$4, phone=$5 WHERE id=$1;`,
+      [id, name, cpf, birthday, phone]
+    );
     res.sendStatus(200);
   } catch (error) {
     res.status(500).send(error.message);
